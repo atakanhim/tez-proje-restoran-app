7fa90a8b21b1a544f1c82b1f6daf6f3762c823fe
Atakan <atakanhim@gmail.com>
2022-10-09 01:46:14 +0300

    temel-atildi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, Login } from "./components";
 
-function App() {
+import { AnimatePresence } from "framer-motion";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence exitBeforeEnter>
+      <div className="w-screen h-screen bg-primary flex justify-center items-center text-base font-bold ">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<Home />} />
+        </Routes>
+      </div>
+    </AnimatePresence>
   );
-}
+};
 
 export default App;
