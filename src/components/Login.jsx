e3337c986438a578d763cc64499951bbdf6437bb
Atakan <atakanhim@gmail.com>
2022-10-11 19:42:46 +0300

    a

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Login = () => {
+  // create login form
+
   return (
-    <div className="relative w-full h-800 bg-black top-24 text-slate-50">
-      login
-    </div>
+    <div className="relative w-full h-800 bg-black top-24 text-slate-50"></div>
   );
 };
 
