a45069e267e89539b67327ae22368eaa483d0de4
Atakan <atakanhim@gmail.com>
2022-10-16 03:37:39 +0300

    a

--- src/components/Chef/ChefHeader.jsx
+++ src/components/Chef/ChefHeader.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 // create header for admin
+const logout = () => {
+  sessionStorage.removeItem("user");
+  window.location.href = "/login";
+};
 const ChefHeader = () => {
   return (
     <div>
@@ -8,6 +12,12 @@ const ChefHeader = () => {
         <div className="flex items-center">
           <div className="text-white font-bold text-2xl">ChefHeader</div>
         </div>
+        <button
+          className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
+          onClick={logout}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
