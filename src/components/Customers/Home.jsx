4bde90f0f59d7d1bc583c9def63995317b045b2c
Atakan <atakanhim@gmail.com>
2022-10-31 17:56:42 +0300

    anasayfa tasrımı yapıldı

--- src/components/Customers/Home.jsx
+++ src/components/Customers/Home.jsx
@@ -1,16 +1,45 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 import { useSelector } from "react-redux";
-var url = window.location.href;
-url = url.split("/"); // url aldım ve böldüm
+
 const Home = () => {
-  const navigate = useNavigate();
-  const { masaNo } = useSelector((state) => state.restoran);
+  const { masaNo, currentCategory, categories, products } = useSelector(
+    (state) => state.restoran
+  );
 
   return (
-    <div className="relative w-full h-800 bg-black top-24 text-slate-50">
-      Home {masaNo}
+    <div className="relative w-full  top-16 bg-gray-300 flex flex-col gap-3 items-center  ">
+      <div className="flex flex-col  text-yellow-600  items-center w-full">
+        <h1 className="text-5xl font-bold">Masa No: {masaNo}</h1>
+        <h1 className="text-5xl font-bold">Kategori: {currentCategory}</h1>
+      </div>
+      <div className="flex flex-col items-center justify-between w-full md:w-1/2 h-1/2 gap-1 p-1">
+        {/* URUNLER BURADA LISTELENECEK DİKEY OLARAK SOLDA KUCUK FOFOTGRAF SAG TARAFTAÜRÜN FİYATI */}
+        {products.map((products) => (
+          <div className="flex flex-col  border-2 border-b-gray-400 rounded-lg w-full">
+            <div className="flex flex-row h-28 justify-center w-full  py-1 px-6 items-center gap-4">
+              <div className=" flex justify-center  flex-col   w-4/5 h-full">
+                <h1 className="text-sm font-bold">{products.product_name}</h1>
+                <h1 className="text-xs font-bold text-textColor w-4/5 h-9 overflow-hidden">
+                  {products.product_description} asdadadadadssadadsad
+                  adsadsadsadsadsadsadsada adsadsadsadsadsadsadsada
+                  adsadsadsadsadsadsadsada adsadsadsadsadsadsadsada
+                  adsadsadsadsadsadsadsada adsadsadsadsadsadsadsada
+                  adsadsadsadsadsadsadsada adsadsadsadsadsadsadsada
+                </h1>
+                <h1 className="text-xs mt-3">{products.product_price} TL</h1>
+              </div>
+              <div className=" flex justify-center items-center flex-col hover:scale-110 transform transition duration-500 ease-in-out  w-1/5 h-full">
+                <img
+                  src={products.product_image}
+                  alt="uploaded"
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
