339f4633691b136eb171abbf55f6957e6a9588e0
Atakan <atakanhim@gmail.com>
2022-11-01 21:49:33 +0300

    aynen yaptik biseyler

---
+++ src/components/CustomCarts/AddToCard.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { useState } from "react";
+
+const AddToCard = ({ selectedProduct }) => {
+  const [urunNotu, setUrunNotu] = useState("");
+  const [urunAdet, setUrunAdet] = useState(1);
+  return (
+    <div className="absolute top-0 left-0 flex w-full min-h-screen h-auto z-50 bg-slate-100">
+      <div className="flex flex-col p-1 w-full items-center gap-2">
+        <div className="relative flex w-full h-56 p-6 border-slate-700 border ">
+          <img
+            src={selectedProduct.product_image}
+            alt="uploaded"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="flex flex-col w-full px-3 border border-slate-900">
+          <div className="flex flex-row p-2 items-center justify-between">
+            <h1 className="text-2xl font-bold">
+              {selectedProduct.product_name}
+            </h1>
+            <h1 className="text-lg font-semibold to-textColor">
+              {selectedProduct.product_price} TL
+            </h1>
+          </div>
+          <h1 className="text-sm font-bold p-2 text-textColor w-4/5 h-9 overflow-hidden">
+            aciklama{selectedProduct.product_description}
+          </h1>
+        </div>
+        <div className="relative flex flex-col mt-auto p-2 h-64 border border-slate-600  w-full">
+          <h1 className="text-lg font-bold">Ürün Notu</h1>
+          <textarea
+            onChange={(e) => setUrunNotu(e.target.value)}
+            className="w-full mt-3 h-full border border-slate-600 p-2"
+            placeholder="Ürünle ilgili bilgi yazabilirsiniz."
+            maxLength={250}
+          ></textarea>
+          <h4 className="text-xs text-textColor ml-auto mr-4 mt-1">
+            {urunNotu.length}/250
+          </h4>
+        </div>
+        <div className="flex flex-row p-2 mt-auto gap-2 h-20 border border-slate-600 w-full">
+          <button className="flex flex-row items-center justify-center w-1/2 h-full border border-slate-600 hover:bg-slate-600 hover:text-white transition duration-500 ease-in-out">
+            <h1 className="text-lg font-bold">Sepete Ekle</h1>
+          </button>
+          <div className="flex flex-row items-center justify-between p-3 w-1/2 h-full border border-slate-600 hover:bg-slate-600 hover:text-white transition duration-500 ease-in-out">
+            <div className="bg-gray-500 w-10 h-10  rounded-full flex items-center justify-center">
+              <h1 className=" text-2xl font-smallbold">-</h1>
+            </div>
+            <div className=" w-10 h-10  rounded-full flex items-center justify-center">
+              <h1 className=" text-2xl font-smallbold">{urunAdet}</h1>
+            </div>
+            <div className="bg-red-500 w-10 h-10  rounded-full flex items-center justify-center">
+              <h1 className=" text-2xl font-smallbold">+</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddToCard;
--- src/components/Customers/Home.jsx
+++ src/components/Customers/Home.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 import { useSelector } from "react-redux";
+import AddToCard from "../CustomCarts/AddToCard";
 
 const Home = () => {
   const { masaNo, currentCategory, categories, products } = useSelector(
     (state) => state.restoran
   );
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const handleProductDetail = (product) => {
+    console.log(product);
+    setSelectedProduct(product);
+  };
 
   return (
-    <div className="relative w-full  top-16 bg-gray-300 flex flex-col gap-3 items-center  ">
+    <div className="  w-full mt-16 bg-gray-300 flex flex-col gap-3 items-center  ">
       <div className="flex flex-col  text-yellow-600  items-center w-full">
         <h1 className="text-5xl font-bold">Masa No: {masaNo}</h1>
         <h1 className="text-5xl font-bold">Kategori: {currentCategory}</h1>
@@ -16,16 +24,16 @@ const Home = () => {
       <div className="flex flex-col items-center justify-between w-full md:w-1/2 h-1/2 gap-1 p-1">
         {/* URUNLER BURADA LISTELENECEK DİKEY OLARAK SOLDA KUCUK FOFOTGRAF SAG TARAFTAÜRÜN FİYATI */}
         {products.map((products) => (
-          <div className="flex flex-col  border-2 border-b-gray-400 rounded-lg w-full">
+          <div
+            className="flex flex-col  border-2 border-b-gray-400 rounded-lg w-full hover:scale-105  transform transition duration-500 ease-in-out "
+            onClick={() => handleProductDetail(products)}
+            key={products.product_id}
+          >
             <div className="flex flex-row h-28 justify-center w-full  py-1 px-6 items-center gap-4">
               <div className=" flex justify-center  flex-col   w-4/5 h-full">
                 <h1 className="text-sm font-bold">{products.product_name}</h1>
                 <h1 className="text-xs font-bold text-textColor w-4/5 h-9 overflow-hidden">
-                  {products.product_description} asdadadadadssadadsad
-                  adsadsadsadsadsadsadsada adsadsadsadsadsadsadsada
-                  adsadsadsadsadsadsadsada adsadsadsadsadsadsadsada
-                  adsadsadsadsadsadsadsada adsadsadsadsadsadsadsada
-                  adsadsadsadsadsadsadsada adsadsadsadsadsadsadsada
+                  {products.product_description}
                 </h1>
                 <h1 className="text-xs mt-3">{products.product_price} TL</h1>
               </div>
@@ -40,6 +48,12 @@ const Home = () => {
           </div>
         ))}
       </div>
+      {selectedProduct && (
+        <AddToCard
+          selectedProduct={selectedProduct}
+          setSelectedProduct={setSelectedProduct}
+        />
+      )}
     </div>
   );
 };
